74b9bd210af2b50fa3b1621248537826faaf2da8
akawil <53502998+akawil@users.noreply.github.com>
2019-11-02 17:58:44 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,129 @@
+const Discord = require('discord.js');  
+const client = new Discord.Client();  
+const moment = require('moment');
+const zalgo = require('zalgolize'); 
+const math = require('math-expression-evaluator'); 
+const figlet = require('figlet');   
+const fs = require('fs');  
+const ms = require('ms');  
+const prefix = '-'
+
+client.on('message', msg => {  
+    if (msg.content === 'باك') { 
+      msg.reply('** :wink: وِلِـكُمِـ ﻧَوِرُتْ   :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === '#اسكت') { 
+      msg.reply('** :wink: شكرا   :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'سلام عليكم') { 
+      msg.reply('** :wink: وِعليكم يلحب منور   :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'شونك') { 
+      msg.reply('** :wink: تمام ونته    :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'بخير') { 
+      msg.reply('** :wink: دوم يلحب   :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'منور') { 
+      msg.reply('** :wink: نورك الصاطع   :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'من هوه مصطفى الشمري') { 
+      msg.reply('** :wink: مصطفى الشمري يوتيوبر على اليوتيوب يتميز بمقاطع مضحكه   :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'لوكي') { 
+      msg.reply('** :wink: لا تجاوز   :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'F1') { 
+      msg.reply('** :wink: ← #«الــشــات-الــعـــام»  يمكنك التواصل والتحدث مع الناس والمشرفين ومصطفى الشمري .
+
+←  #⚡فعاليات⚡  يمكنك لعب العاب وربح جوائز قيمه مع القائمين او المشرفين ، tada.
+
+← #«videos-الــفــيــديــوهــات»  يمكنك رؤية مقاطع  اليوتيوبرز  في السيرفر .
+
+← #«news-الاخبـــــــــار»  يمكنك رؤيه كل االأخبار المتعلقة بالسيرفر  .
+
+←  #«العاب-بوت»  يمكنك لعب العاب متنوعه مع الروبوت (( البوت )) robot .
+
+← #«commands-الاوامــر» يمكنك رؤيه مستوى تفاعلك وكسب الالوان او رتب sparkles
+
+← #«laws-القــــــــوانين»  يمكنك قراءة قوانين السيرفر  .
+
+←  #«pictures-الصــــــــور» يمكنك قراءة ونشر نكت مع رؤية خواطر ونشر رسائل بشكل متكرر ونشر صور .
+
+إذا كان لديك سؤال آخر لا تترد بطرحه علينا ، وشكراً :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'بوت') { 
+      msg.reply('** :wink: تفضل عزيزي   :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'F2') { 
+      msg.reply('** :wink:   مرحــبــا بــكــم فــي ســيــرفـــر مصطفى الشمري 
+
+               هذا السيرفر خاص بالفعاليات والتواصل وقضاء وقت ممتع معناً 
+ نتمنى من الجميع عدم مخالفة القوانين يرجى من الجميع قراءة القوانين والتقيد بها شاكرين لكم تفهمكم  :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'F3') { 
+      msg.reply('** :wink: عزيزي رجاء عدم ارسال الكلمات البذيئه   :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'F4') { 
+      msg.reply('** :wink: عزيزي عدم اثارة المشاكل الخاصه بك في السيرفر   :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'F5') { 
+      msg.reply('** :wink: ممنوع طلب لرتب داخل السيرفر الرتبه فقط تعطى الى الشخص المتفاعل   :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'مختصر السيرفر') { 
+      msg.reply('** :wink: السيرفر فقط للضحك والونسه والعب    :sparkling_heart:**'); 
+    }
+  });
+  
+  client.on('message', msg => {  
+    if (msg.content === 'F6') { 
+      msg.reply('** :wink: وعزيزي رجاء عدم التكلم في امور السياسه   :sparkling_heart:**'); 
+    }
+  });
+
+
+
+
+client.login(process.env.BOT_TOKEN);
